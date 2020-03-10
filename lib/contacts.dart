81dd1652021c6b6444d50addda392226238c998a
Administrator <marat.gubaidullin@elementai.com>
2020-03-10 00:22:19 -0400

    prepapre

---
+++ lib/contacts.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'chat.dart';
+
+class ContactScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          backgroundColor: Colors.red,
+          title: new Text("Christo Hub"),
+        ),
+        body: Scrollbar(
+            child: ListView(
+                padding: EdgeInsets.symmetric(vertical: 8),
+                children: [
+                  for (int index = 1; index < 21; index++)
+                    ListTile(
+                      leading: ExcludeSemantics(
+                        child: CircleAvatar(child: Text('$index')),
+                      ),
+                      title: Text(("rip")),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => ChatScreen()),
+                        );
+                      },
+                    )
+                ]
+            )
+        )
+
+    );
+  }
+}
